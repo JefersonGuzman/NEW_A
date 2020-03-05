95b2a831c2180d5dc452eb26de201077288dfaac
JefersonGuzman <Jefersonlozano07@gmail.com>
2020-03-05 11:57:24 -0500

    agregamos archivo js del menu

---
+++ menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+  /* CLick BTN (Open and Close) */
+  $('.button-nav--toggle').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('is-showNavMob');
+  });
+});
